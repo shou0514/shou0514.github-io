3d6f22381b8afec491f590c27b3033a883ce02ac
shou0514 <87031849+Highdog66666666@users.noreply.github.com>
2023-09-12 15:45:30 +0800

    Create cin_s.cpp

---
+++ C++/cin_s.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"姓名\t座號\t電話\n";
+    cout <<"張山田\t11\t0224567890\n";
+    cout <<"謝明山\t22\t0229876543\n";
+    cout <<"田秀水\t33\t0221234567\n";
+    system ("pause");
+    return 0;
+}
