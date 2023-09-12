f415bdc4ee84b79b991e43fd32daf163ee0d979b
shou0514 <87031849+Highdog66666666@users.noreply.github.com>
2023-09-12 15:45:22 +0800

    Create address_s.cpp

---
+++ C++/address_s.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string score;
+    cout << "請輸入英文成績(0-100):";
+    cin  >> score;
+    cout << endl << "你的英文成績為 " << score <<" 分 ";
+    system ("pause");
+    return 0;
+}
