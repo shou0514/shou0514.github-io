efe9615432598c219f0bc28d04ed578f1ee94925
shou0514 <87031849+Highdog66666666@users.noreply.github.com>
2023-09-12 15:52:51 +0800

    Create complex.cpp

---
+++ C++/complex.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    float result , n,m,k;
+    cout << "請輸入第一個數：";
+    cin >>n;
+    result +=n;
+    cout << "請輸入第二個數：";
+    cin >>m;
+    result +=m;
+    cout << "請輸入第三個數：";
+    cin >>k;
+    result +=k;
+    cout << "三個數的總和為：" <<result <<"\n";
+    system ("pause");
+    return 0;
+}
