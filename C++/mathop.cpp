d8a1d4edcf48d5bbcfa3629e643b5a7ee0c670da
shou0514 <133513724+shou0514@users.noreply.github.com>
2023-09-25 15:39:11 +0800

    Create mathop.cpp

---
+++ C++/mathop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+float a,b;
+cout <<"請輸入第一個數:";
+cin >>a;
+cout <<"請輸入第二個數:";
+cin >>b;
+cout<<"兩數之合為:"<<a+b <<"\n";
+system ("pause");
+return 0;
+}
