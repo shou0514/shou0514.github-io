2a30809edb19c71b26c6281b20fb253463fd5b03
shou0514 <133513724+shou0514@users.noreply.github.com>
2023-09-25 15:40:12 +0800

    Create test.cpp

---
+++ C++/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "李明玉" << " 座號為11號"<< " 電話為0224567890" <<endl;
+    cout << "張山田" << " 座號為22號"<< " 電話為0229876543" <<endl;
+    cout << "黃大衛" << " 座號為33號"<< " 電話為0221234567" <<endl;
+    system("pause");
+    return 0;
+}
