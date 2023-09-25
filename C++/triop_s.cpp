0b9dc9c7a10dade27a613f859f1f0e367a42e838
shou0514 <133513724+shou0514@users.noreply.github.com>
2023-09-25 15:39:57 +0800

    Create triop_s.cpp

---
+++ C++/triop_s.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float top,bottom ,height,area;
+    cout<<"請輸入梯形的上底（公分）：";
+    cin >>top;
+    cout<<"請輸入梯形的下底(公分):";
+    cin >>bottom;
+    cout <<"請輸入梯形的高（公分）：";
+    cin >>height;
+    area = (top+bottom)*height / 2;
+    cout<<"梯形的面積為："<<area <<" 平方公分";
+    system ("pause");
+    return 0;
+}
